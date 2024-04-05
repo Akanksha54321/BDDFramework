7d0abb67df2f87b23f070300b87f0d035c0601a8
Akanksha <akanksha.gupta2120@gmail.com>
2024-04-05 14:00:29 -0400

    new changes to main

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
      //   features=".//Features//Login.feature",
-    //    features=".//Features//Customers.feature",
+       features=".//Features//Customers.feature",
      //     features=".//Features/",
     //      features={".//Features//Login.feature",".//Features//Customers.feature"},
         glue="stepDefinitions",
@@ -16,8 +16,8 @@ import org.junit.runner.RunWith;
   //     tags={"@regression"},  //To execute regression scenarios
   //     tags={"@Sanity,@regression"},  //To execute both Sanity regression scenarios
  //   		   tags={"@Sanity,@regression"},  //To execute scenthose have both Sanity and Regression tags those are rarely used
- //       dryRun=false,
-//        monochrome=true,
+        dryRun=false,
+        monochrome=true,
         plugin= {"pretty","html:Reports"}
 )
 public class TestRun {
