32bae5ae40986aa91c40e5e5426c19c47b767e4e
Akanksha <akanksha.gupta2120@gmail.com>
2024-04-05 14:03:12 -0400

    new changes n delete logs1 n log2 Branch1

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
      //   features=".//Features//Login.feature",
-    //    features=".//Features//Customers.feature",
+        features=".//Features//Customers.feature",
      //     features=".//Features/",
     //      features={".//Features//Login.feature",".//Features//Customers.feature"},
         glue="stepDefinitions",
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
   //     tags={"@regression"},  //To execute regression scenarios
   //     tags={"@Sanity,@regression"},  //To execute both Sanity regression scenarios
  //   		   tags={"@Sanity,@regression"},  //To execute scenthose have both Sanity and Regression tags those are rarely used
- //       dryRun=false,
+        dryRun=false,
         monochrome=true,
         plugin= {"pretty","html:Reports"}
 )
